fb5023a58014dd58d890c3dca7465892ddca2dd3
Krzysztof-Skr <krzysztof.skr@wp.pl>
2017-05-06 13:25:41 +0200

    Add files via upload

---
+++ includ/kontakt.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+	<link rel="stylesheet" type="text/css" href="css/kontakt.css">
+	
+</head>
+<body>
+	<?php include 'menu.html'; ?>
+		<div class="container">
+			<div class="jumbotron">
+				<div class="row">
+					<div class="col-sm-12 col-md-4">
+						<h1>Kontakt</h1>
+						<address>
+							Project Design Dariusz Skwierczyński<br/>
+							ul. Narutowicza 62<br/>
+							08-110 Siedlce<br/>
+							woj. mazowieckie <br/>
+						</address>
+					</div>
+					<div class="col-sm-12 col-md-5">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2057.27305642784!2d22.28611295639297!3d52.17935176993602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471f5bafaf8fc87b%3A0xdbe1e712fb829c1b!2sGabriela+Narutowicza+62%2C+08-110+Siedlce!5e0!3m2!1spl!2spl!4v1491167436143" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Bootstrap core JavaScript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="js/jquery-3.2.0.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+		<script src="js/bootstrap.min.js"></script>
+
+		<script src="js/jquery-3.2.0.min.js"></script>
+		<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+	</body>
+</html>
---
+++ includ/menu2.html
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index.html">Project Design</a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="index.html">O firmie</a></li>
+					<li><a href="#oferta">Oferta</a></li>
+				<!-- rozwijane menu -->
+					<li class="dropdown">
+			            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Oferta <span class="caret"></span></a>
+			              <ul class="dropdown-menu">
+			                <li><a href="szafy.html">Szafy</a></li>
+			                <li><a href="#">Garderoby</a></li>
+			                <li><a href="#">Meble łazienkowe</a></li>
+			                <li><a href="#">Kuchnie</a></li>
+			                <li><a href="#">Meble inne</a></li>
+			              </ul>
+			            </li>
+	            <!-- koniec rozwijane menu -->
+					<li><a href="galeria.html">Galeria</a></li>
+					<li><a href="kontakt.html">Kontakt</a></li>
+				</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</nav>
